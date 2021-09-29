24d9b3a31a74f59fcc3c28637e4addaf1a8a628b
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-09-29 10:48:28 -0300

    Fix auth to retrieve token

--- src/auth.js
+++ src/auth.js
@@ -4,15 +4,26 @@ let axiosConfig = {
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
     'Access-Control-Allow-Origin': '*',
+    'Host': 'localhost:4200',
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0",
+    'Accept': ['application/json', 'text/plain', '*/*',],
+    'Content-Type': 'application/json',
+    'Content-Length': 51,
+    'Origin': 'http://localhost:4200',
+    'Connection': 'keep-alive',
+    'Referer': 'http://localhost:4200/auth/login',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
   }
 };
 
 const user = {
-  email: process.env.EMAIL,
-  password: process.env.PASSWORD,
+  'email': process.env.EMAIL,
+  'password': process.env.PASSWORD,
 };
 
 const errors = [];
 
-const func = () => axios.post(`${ process.env.FULL_PATH }/tokens`, JSON.stringify(user), axiosConfig);
+const func = () => axios.post('http://localhost:80/api/v1/tokens', JSON.stringify(user), axiosConfig);
 module.exports = func;
